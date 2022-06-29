abd7ff4bac6b47279698e5a57b2548cc99ab2845
ribamarf01 <ribamar.f01@gmail.com>
2022-06-28 21:47:57 -0300

    lettuce component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Bun from "./components/Bun"
+import Lettuce from './components/Lettuce'
 
 const App = () => {
   return(<>
     <div className="flex flex-col items-center justify-center h-screen w-full">
       <Bun isUpperPart />
+      <div className="py-2"></div>
+      <Lettuce />
       <div className="py-2"></div>
       <Bun />
     </div>
---
+++ src/components/Lettuce.tsx
@@ -0,0 +1,11 @@
+const Lettuce = () => {
+
+  return (<>
+    <div 
+      className="h-8 w-72 bg-lettuce rounded-t-3xl border-4 border-black"
+    ></div>
+  </>)
+
+}
+
+export default Lettuce
