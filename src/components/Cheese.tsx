889a59c7fbbf1d35213af9b807aa4f84dc7c81b0
ribamarf01 <ribamar.f01@gmail.com>
2022-06-29 17:58:07 -0300

    cheez changes

--- src/components/Cheese.tsx
+++ src/components/Cheese.tsx
@@ -1,9 +1,14 @@
 const Cheese = () => {
 
   return (<>
-    <div 
-      className="h-8 w-80 bg-cheese rounded-t-3xl border-4 border-black"
-    ></div>
+    <div className="h-8 w-80 bg-cheese rounded-t-3xl border-4 border-black relative">
+      <div className="h-8 w-7 bg-cheese rounded-b-3xl rounded-t-lg border-4 border-black border-t-transparent absolute top-6 -left-1 z-10"></div>
+
+      <div className="h-4 w-7 bg-cheese rounded-b-3xl border-4 border-black border-t-transparent absolute top-6 right-20 z-10"></div>
+      <div className="h-10 w-8 bg-cheese rounded-b-3xl border-4 border-black border-t-transparent absolute top-6 right-24 z-10"></div>
+
+      <div className="h-8 w-7 bg-cheese rounded-b-3xl rounded-t-lg border-4 border-black border-t-transparent absolute top-6 -right-1"></div>
+    </div>
   </>)
 
 }
